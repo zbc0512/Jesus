61f6c1446bf4c90f6788e218157ec80b63caa1b7
ZhangBaicheng <zbc0512@163.com>
2017-08-27 19:27:53 +0800

    remove import

--- src/main/java/io/zbc/jesus/controller/HolyBibleController.java
+++ src/main/java/io/zbc/jesus/controller/HolyBibleController.java
@@ -1,7 +1,5 @@
 package io.zbc.jesus.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +12,7 @@ public class HolyBibleController {
 
 	@Autowired
 	HolyBibleHGBRepository holyBibleHGBRepository;
-	
+
 	@RequestMapping("/hgb")
 	public Iterable<HolyBibleHGB> getToday() {
 		return holyBibleHGBRepository.findAll();
